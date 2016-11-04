5b0f453f190b68eecc4d48c4c6bbf6ae88b6e872
1213306369 <1213306369@qq.com>
2016-11-04 23:10:08 +0800

    Add files via upload

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -5,7 +5,10 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -18,22 +21,30 @@ import java.util.Scanner;
 public class FilterByTime {
 	
 	public static void main(String[] args) throws ParseException, FileNotFoundException{
-		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
-		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
+		Locale locale = Locale.US;
+		SimpleDateFormat regularFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss",locale);
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date beginDate = outputFormat.parse("2015-12-31 18:00:00");
+		Date endDate = outputFormat.parse("2015-12-31 19:00:00");
 		String filePath = "./access.log";
 		FileInputStream inputStream = new FileInputStream(filePath);
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
 		while (scanner.hasNext()){
-			// 对每行进行处理
 			String line = scanner.nextLine();
-			// 切分获取IP，Time
-			String [] alt = line.split(" ");
-			String strIp = alt[0];
-			String strTime = alt[3].substring(1,alt[3].length()-1);
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+			String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[([^ ]*) [^ ]*\\] \"[^ ]+ [^ ]+.*";
+			Pattern r = Pattern.compile(pattern);
+			// 对每行进行处理
+			Matcher m = r.matcher(line);
+			if(m.find()){
+				Date date = regularFormat.parse(m.group(2));
+				if(date.before(endDate)&&date.after(beginDate)){
+					// 切分获取IP，Time
+					String strIp = m.group(1);
+					String strTime = outputFormat.format(date);
+					// 对在时间区间内的数据进行输出
+					System.out.println(strIp + "\t" + strTime);
+				}
+			}
 		}
 	}
-	
 }
